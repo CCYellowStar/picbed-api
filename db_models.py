34f99f8b7c3c370793f328f476da3c0722aa83e9
CCYellowStar <47291058+CCYellowStar@users.noreply.github.com>
2023-03-19 09:26:46 +0000

    orm

---
+++ db_models.py
@@ -0,0 +1,40 @@
+from tortoise import fields
+from tortoise.models import Model
+
+
+class Emojis(Model):
+    id = fields.IntField(pk=True)
+    url = fields.CharField(max_length=255)
+    upload_time = fields.CharField(max_length=255)
+    uploader = fields.CharField(max_length=255)
+  
+    class Meta:
+        table =  f"{__name__}.emojis"
+
+class Tags(Model):
+    id = fields.IntField(pk=True)
+    name = fields.CharField(max_length=255)
+    emoji_count = fields.IntField(default=0)
+    search_count = fields.IntField(default=0)
+  
+    class Meta:
+        table =  f"{__name__}.tags"
+
+class EmojiTag(Model):
+    id = fields.IntField(pk=True)
+    emoji = fields.ForeignKeyField('models.Emojis', related_name='emoji_tag')
+    tag = fields.ForeignKeyField('models.Tags', related_name='emoji_tag')
+  
+    class Meta:
+        table =  f"{__name__}.emoji_tag"
+
+class Submissions(Model):
+    id = fields.IntField(pk=True)
+    url = fields.CharField(max_length=255)
+    tag = fields.CharField(max_length=255)
+    submitter = fields.CharField(max_length=255)
+    submission_date = fields.CharField(max_length=255)
+    status = fields.IntField(default=0)
+  
+    class Meta:
+        table =  f"{__name__}.submissions"
--- main.py
+++ main.py
@@ -1,51 +1,14 @@
 from flask import Flask, request, jsonify
-import sqlite3
+import asyncio
+from db_models import Emojis, Tags, EmojiTag,Submissions
+from tortoise import Tortoise
 import requests
 import random
 from datetime import datetime
 from flask_cors import CORS
-app = Flask(__name__, static_folder='static',template_folder='static',static_url_path='/static')
+app = Flask(__name__)
 CORS(app, resources=r'/*')
 # Connect to the MySQL database
-conn = sqlite3.connect("my_emojis")
-cursor = conn.cursor()
-cursor.execute('''SELECT COUNT(*) FROM sqlite_master WHERE type='table' AND name='emojis' ''')
-if cursor.fetchone()[0] == 0:
-  cursor.execute('''
-      CREATE TABLE emojis (
-          id INTEGER PRIMARY KEY AUTOINCREMENT,
-          url TEXT NOT NULL,
-          upload_time TEXT NOT NULL,
-          uploader TEXT NOT NULL
-      )
-  ''')
-  cursor.execute('''
-      CREATE TABLE tags (
-          id INTEGER PRIMARY KEY AUTOINCREMENT,
-          name TEXT NOT NULL,
-          emoji_count INTEGER NOT NULL DEFAULT 0,
-          search_count INTEGER NOT NULL DEFAULT 0
-      )
-  ''')
-  cursor.execute('''
-      CREATE TABLE emoji_tag (
-          id INTEGER PRIMARY KEY AUTOINCREMENT,
-          emoji_id INTEGER NOT NULL,
-          tag_id INTEGER NOT NULL,
-          FOREIGN KEY (emoji_id) REFERENCES emojis (id),
-          FOREIGN KEY (tag_id) REFERENCES tags (id)
-      );
-  ''')
-  cursor.execute('''
-    CREATE TABLE submissions (
-        id INTEGER PRIMARY KEY AUTOINCREMENT,
-        url TEXT NOT NULL,
-        tag TEXT NOT NULL,
-        submitter TEXT NOT NULL,
-        submission_date TEXT NOT NULL,
-        status INTEGER NOT NULL DEFAULT 0
-    )
-''')
 
 
 
@@ -55,10 +18,8 @@ def index():
 
 
 @app.route('/upload', methods=['POST'])
-def upload():
+async def upload():
   success = False
-  conn = sqlite3.connect('my_emojis')
-  cursor = conn.cursor()
   data = request.get_json()
   for da in data:
     pic = da['base64']
@@ -78,80 +39,48 @@ def upload():
                           data=d)
       url = res.text
       upload_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-      cursor.execute("INSERT INTO submissions (url,tag, submitter, submission_date,status) VALUES (?, ?, ?, ?, ?)", (url, tag_str,uploader,upload_time,0))
-      """
-      for tag in tags:
-        cursor.execute("SELECT * FROM tags WHERE tags = ?", (tag, ))
-        result = cursor.fetchall()
-        if result is not None:
-          tag_id = result[0]
-          emoji_count = result[2]
-          cursor.execute("UPDATE tags SET emoji_count = ? WHERE id = ?", (emoji_count + 1, tag_id))"""
-      conn.commit()
-      success = True
+      if url !="重复上传":
+      
+        await Submissions.create(url=url, tag=tag_str, submitter=uploader, submission_date=upload_time, status=0)
+  
+        success = True
+      else:
+        success = False
     except:
       success = False
       pass
-  conn.commit()
-  conn.close()
-  message = '上传成功！' if success else '上传失败，请重试！'
+  message = 'success' if success else 'failed'
   print(message)
-  return jsonify({'success': success, 'message': message})
+  return message
 
 
 @app.route('/random_emotic', methods=['POST'])
 @app.route('/random_emotic/<keyword>', methods=['GET'])
-def emoji(keyword):
-  conn = sqlite3.connect('my_emojis')
-  cursor = conn.cursor()
+async def emoji(keyword):
   if request.method == 'GET':
-    cursor.execute("SELECT * FROM tags WHERE name = ?", (keyword , ))
-    result = cursor.fetchall()
-    if len(result) > 0:
-      tag_id = result[0]
-      cursor.execute("SELECT * FROM emoji_tag WHERE tag_id = ?", (tag_id , ))
-      result1 = cursor.fetchall()
-      if len(result1) > 0:
-        emoji_id = result1[random.randint(0, len(result1) - 1)][1]
-        url=cursor.execute("SELECT * FROM emojis WHERE id = ?", (emoji_id , )).fetchall()[0][1]
-        conn.close()
+    tag = await Tags.filter(name=keyword).first()
+    if tag:
+      emoji_ids = await EmojiTag.filter(tag_id=tag.id).all()
+      if emoji_ids:
+        emoji_id = emoji_ids[random.randint(0, len(emoji_ids) - 1)].emoji
+        url=await Emojis.filter(id=emoji_id).first().url
         return url
       else:
-        cursor.execute("SELECT * FROM emojis")
-        data = cursor.fetchall()
-        conn.close()
-        return data[random.randint(0, len(data) - 1)][1]
+        return "failed"
     else:
-      cursor.execute("SELECT * FROM emojis")
-      data = cursor.fetchall()
-      conn.close()
-      return data[random.randint(0, len(data) - 1)][1]
+      return "failed"
 
   if request.method == 'POST': #再解决屏蔽词
-    data = request.get_json()
-    keyword=data['keyword']
-    blocked_tags=data['forbidden_tag']
-    cursor.execute("SELECT * FROM tags WHERE name = ?", (keyword , ))
-    result = cursor.fetchall()
-    if len(result) > 0:
-      tag_id = result[0]
-      cursor.execute("SELECT * FROM emoji_tag WHERE tag_id = ?", (tag_id , ))
-      result1 = cursor.fetchall()
-      if len(result1) > 0:
-        emoji_id = result1[random.randint(0, len(result1) - 1)][1]
-        url=cursor.execute("SELECT * FROM emojis WHERE id = ?", (emoji_id , )).fetchall()[0][1]
-        conn.close()
-        return url
-      else:
-        cursor.execute("SELECT * FROM emojis")
-        data = cursor.fetchall()
-        conn.close()
-        return data[random.randint(0, len(data) - 1)][1]
-    else:
-      cursor.execute("SELECT * FROM emojis")
-      data = cursor.fetchall()
-      conn.close()
-      return data[random.randint(0, len(data) - 1)][1]
-    
+    pass
+
+async def init_db():
+    await Tortoise.init(
+        db_url='sqlite://my_emojis.db',
+        modules={'models': [__name__]}
+    )
+    await Tortoise.generate_schemas()   
 if __name__ == '__main__':
-  app.run(host='0.0.0.0', port=81)
+    asyncio.run(init_db())
+    app.run(host='0.0.0.0', port=81)
+
+
